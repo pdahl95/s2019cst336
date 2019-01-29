240c9ccfdbdef5a5dca1716fc382fa27b7a38b7d
Dahl_Cloud9_Workspace <pdahl@csumb.edu>
2019-01-29 01:00:42 +0000

    added lab 1 index.html

--- index.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title> CST 336 - Pernille Dahl </title>
-    </head>
-    <body>
-        This is a test! This is a second test to see that everything gets updated!
-    </body>
-</html>
