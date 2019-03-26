237c177e1111557a67896055382b5f8643f390b2
Dahl_Cloud9_Workspace <pdahl@csumb.edu>
2019-03-26 21:37:17 +0000

    lab6 updated with heruko

--- labs/6/dbConnection.php
+++ labs/6/dbConnection.php
@@ -5,10 +5,18 @@ function getDatabaseConnection($dbname = 'ottermart'){
     //mysql://uvrmbwpgnycuyc1s:smg0ff2qluhi96yz@er7lx9km02rjyf3n.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/uvwpgd9q61wn8fld
     
     // C9 connection string
-
     $host = 'localhost';
     $username = 'peerni95';
     $password = '';
+    
+      if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
+        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        $host = $url["host"];
+        $dbname = substr($url["path"], 1);
+        $username = $url["user"];
+        $password = $url["pass"];
+    } 
+
     
     $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     
