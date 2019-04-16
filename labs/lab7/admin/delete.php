b08927fd3627ce3a7062b24b9d118dfdf03b39b3
Dahl_Cloud9_Workspace <pdahl@csumb.edu>
2019-04-16 05:38:41 +0000

    added delete from db

--- labs/lab7/admin/delete.php
+++ labs/lab7/admin/delete.php
@@ -1,10 +1,23 @@
 <?php
 
     session_start();
-    
 
     if (!isset($_SESSION['email'])){
       header("Location: login.html");
+    }
+    
+    include '../dbConnection.php';
+    $conn = getDatabaseConnection("ottermart");
+    
+        if(isset($_POST['submit'])){
+        $prodID = $_POST['prodId'];
+       
+        $sql = "DELETE FROM om_product WHERE productId = $prodID;";
+        $stmt = $conn->prepare($sql);
+        $response = $stmt->execute();
+        
+        echo json_encode($response);
+        exit(0);
     }
 
 ?>
@@ -54,7 +67,7 @@
             <div id="cont"> 
                 What product do you want to delete? <br> 
                 Please provide the Product ID below: <br> 
-                <input type="text" name="prodId"/> <br> 
+                <input id="prod" type="text" name="prodId"/> <br> 
                 
                 <button id="delete" class="btn btn-danger"> Delete Product </button>
                 
@@ -67,7 +80,9 @@
             <button id="btn" class="btn btn-danger">Logout</button>
         </main>
 
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+
+        <!--<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>-->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
@@ -79,12 +94,30 @@
             }); 
             
             $("#delete").on("click", function() {
-                alert("Sure you want to delete this product?");
+                confirm("Sure you want to delete this product?");
+                $.ajax({
+                    type: "POST", 
+                    url: "delete.php",
+                    dataType: "json",
+                    data: { 
+                      'submit': '', 
+                      "prodId": $("[name='prodId']").val(),
+                     
+                    },
+                   success: function(data,status){
+                       alert("Success!");
+                   }, 
+                   error: function (){
+                        alert("Fail!");
+                    }
+                });
             });
             
             $("#goBack").on("click", function() {
                 window.location = "index.php";
             }); 
+            
+            
             
      
             
