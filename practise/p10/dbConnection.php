27c88f6cbe2be9692626b5844120eb89d79ef07c
Dahl_Cloud9_Workspace <pdahl@csumb.edu>
2019-04-29 19:55:13 +0000

    update php db conn

--- practise/p10/dbConnection.php
+++ practise/p10/dbConnection.php
@@ -1,14 +1,15 @@
-<?php
-
+ <?php
+ 
 
 function getDatabaseConnection($dbname){
+    // Heroku connection string:
+    //mysql://uvrmbwpgnycuyc1s:smg0ff2qluhi96yz@er7lx9km02rjyf3n.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/uvwpgd9q61wn8fld
     
-    $host = 'localhost';//cloud 9
-    //$dbname = 'tcp';
+    // C9 connection string
+    $host = 'localhost';
     $username = 'root';
     $password = '';
     
-    //using different database variables in Heroku
     if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
         $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
         $host = $url["host"];
@@ -16,18 +17,45 @@ function getDatabaseConnection($dbname){
         $username = $url["user"];
         $password = $url["pass"];
     } 
+
     
-    //creates db connection
     $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     
-    //display errors when accessing tables
     $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    return $dbConn;
     
+    return $dbConn;
 }
 
 
 
 
-?>
+// function getDatabaseConnection($dbname){
+    
+//     $host = 'localhost';//cloud 9
+//     //$dbname = 'tcp';
+//     $username = 'root';
+//     $password = '';
+    
+//     //using different database variables in Heroku
+//     if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
+//         $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+//         $host = $url["host"];
+//         $dbname = substr($url["path"], 1);
+//         $username = $url["user"];
+//         $password = $url["pass"];
+//     } 
+    
+//     //creates db connection
+//     $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    
+//     //display errors when accessing tables
+//     $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+//     return $dbConn;
+    
+// }
+
+
+
+
+ ?>
